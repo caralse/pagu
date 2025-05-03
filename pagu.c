96d85411db60120e7dadbd6b62c087d95cdf1f44
caral <carloalbertoserra@gmail.com>
2025-05-03 11:55:45 +0200

    Adds Page Down/Up

--- pagu.c
+++ pagu.c
@@ -13,7 +13,16 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-enum editor_key { ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN };
+enum editor_key {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
 
 typedef struct {
     int cx, cy;
@@ -116,15 +125,45 @@ int e_read_key() {
             return '\x1b';
         }
         if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                    case '1':
+                    case '7':
+                        return HOME_KEY;
+                    case '4':
+                    case '8':
+                        return END_KEY;
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    }
+                }
+            } else {
+                switch (seq[1]) {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
+                }
+            }
+        } else if (seq[0] == 'O') {
             switch (seq[1]) {
-            case 'A':
-                return ARROW_UP;
-            case 'B':
-                return ARROW_DOWN;
-            case 'C':
-                return ARROW_RIGHT;
-            case 'D':
-                return ARROW_LEFT;
+            case 'H':
+                return HOME_KEY;
+            case 'F':
+                return END_KEY;
             }
         }
         return '\x1b';
@@ -193,6 +232,20 @@ void e_process_keypress() {
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
+
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+    case END_KEY:
+        E.cx = E.screen_cols - 1;
+        break;
+
+    case PAGE_UP:
+    case PAGE_DOWN: {
+        int times = E.screen_rows;
+        while (times--)
+            e_move_cursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    } break;
 
     case ARROW_UP:
     case ARROW_DOWN:
