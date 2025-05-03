1cb2ba36608c3bf2acd0901f0d93b98741d6af53
caral <carloalbertoserra@gmail.com>
2025-05-03 10:15:08 +0200

    Adds Dynamic Buffer

--- pagu.c
+++ pagu.c
@@ -4,27 +4,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-struct termios orig_termios;
+typedef struct {
+    int screen_rows;
+    int screen_cols;
+    struct termios orig_termios;
+} editorConfig;
+
+editorConfig E;
 
 // terminal
 void enable_raw_mode(void);
 void disable_raw_mode(void);
 void die(const char *);
 char e_read_key();
+int get_window_size(int *, int *);
+int get_cursor_pos(int *, int *);
 
-// init
+// append buffer
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void ab_append(struct abuf *ab, const char *s, int len);
+void ab_free(struct abuf *ab);
+
+// input
 void e_process_keypress();
 
 // output
 void e_clear();
-void e_draw_rows();
+void e_draw_rows(struct abuf *);
+
+// init
+void e_init();
 
 int main() {
+    e_init();
     enable_raw_mode();
 
     while (1) {
@@ -36,12 +60,12 @@ int main() {
 }
 
 void enable_raw_mode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
         die("tcgetattr");
     }
     atexit(disable_raw_mode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -56,7 +80,7 @@ void enable_raw_mode() {
 }
 
 void disable_raw_mode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
         die("tcsetattr");
     }
 }
@@ -78,7 +102,58 @@ char e_read_key() {
     return c;
 }
 
-/*** input ***/
+int get_window_size(int *rows, int *cols) {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+            return -1;
+        }
+        return get_cursor_pos(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+int get_cursor_pos(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+        return -1;
+    }
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if (buf[i] == 'R')
+            break;
+        i++;
+    }
+
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[')
+        return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+        return -1;
+    return 0;
+}
+
+// append buffer
+void ab_append(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL)
+        return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void ab_free(struct abuf *ab) { free(ab->b); }
+
+// input
 void e_process_keypress() {
     char c = e_read_key();
     switch (c) {
@@ -90,17 +165,27 @@ void e_process_keypress() {
     }
 }
 
-/*** output ***/
+// output
 void e_clear() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
-    e_draw_rows();
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
+    ab_append(&ab, "\x1b[2J", 4);
+    ab_append(&ab, "\x1b[H", 3);
+    e_draw_rows(&ab);
+    ab_append(&ab, "\x1b[H", 3);
+    write(STDOUT_FILENO, ab.b, ab.len);
+    ab_free(&ab);
 }
 
-void e_draw_rows() {
-    int y;
-    for (y = 0; y < 24; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+void e_draw_rows(struct abuf *ab) {
+    for (int y = 0; y < E.screen_rows - 1; y++) {
+        ab_append(ab, "~\r\n", 3);
+    }
+    ab_append(ab, "~", 1);
+}
+
+// init
+void e_init() {
+    if (get_window_size(&E.screen_rows, &E.screen_cols) == -1) {
+        die("get_window_size");
     }
 }
